0aa6a6ccb2a08688619d3b895f82403a76289409
MudassirASyed <mudassiralishah555@gmail.com>
2025-02-11 17:13:55 +0500

    email functionality done for organizer ,conference creation and status updation

--- ConfHub-backend/config/plugins.ts
+++ ConfHub-backend/config/plugins.ts
@@ -9,4 +9,20 @@ export default () => ({
         },
       },
     },
+    email: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: 'smtp.gmail.com', // e.g., 'smtp.gmail.com'
+        port: 587, // or 465 for secure connections
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: 'mudassiralishah555@gmail.com', // your email address
+          pass: 'shah5555',    // your email password
+        },
+      },
+      settings: {
+        defaultFrom: 'mudassiralishah555@gmail.com', // default sender address
+        defaultReplyTo: 'mudassiralishah555@gmail.com', // default reply-to address
+      },
+    },
   });
