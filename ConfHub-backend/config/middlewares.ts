b5345bab4ccc78854979f10cde00325cfc27f81f
MudassirASyed <mudassiralishah555@gmail.com>
2025-08-10 17:02:49 +0500

    deplyement changes

--- ConfHub-backend/config/middlewares.ts
+++ ConfHub-backend/config/middlewares.ts
@@ -1,8 +1,28 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io'],
+          'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['*'], // We'll update this later with your frontend URL
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
